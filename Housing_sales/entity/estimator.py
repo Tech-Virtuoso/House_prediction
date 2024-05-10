0422df9c9a2386b88e2b0aa3106dc301c2451834
Devendrasingh Solanki” <dsoulanki49@gmail>
2024-05-10 22:45:38 +0530

    estimator update

--- Housing_sales/entity/estimator.py
+++ Housing_sales/entity/estimator.py
@@ -8,8 +8,8 @@ from Housing_sales.logger import logging
 
 class TargetValueMapping:
     def __init__(self):
-        self.Certified:int = 0
-        self.Denied:int = 1
+        self.yes:int = 0
+        self.no:int = 1
     def _asdict(self):
         return self.__dict__
     def reverse_mapping(self):
